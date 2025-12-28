1daf64eff6095451f2696cb7a524f4858457c375
Heber Sandoval <he3ers@gmail.com>
2025-12-28 09:21:45 -0600

    Fixed PathError when using wild card (*).

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 8000;
 
 // Catch-all 404 handler (use a pathless middleware to avoid path parsing errors)
+// app.use('*', (req, res) => {
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
 });
