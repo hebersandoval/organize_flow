6b001e3d7c3a08884d8dc60098a39ac4ae7763e6
Heber Sandoval <he3ers@gmail.com>
2026-01-07 07:22:29 -0600

    Tested React with a sample app.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,22 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [visibility, setVisibility] = useState(true);
+
+    const toggleDiv = () => {
+        setVisibility(!visibility);
+    };
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <div>
+            {visibility && (
+                <div id="myDiv">
+                    <p>Hello world</p>
+                </div>
+            )}
+
+            <button onClick={toggleDiv}>Toggle "div"</button>
+        </div>
     );
 }
 
