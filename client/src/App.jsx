60995f3be04b0db4e17a7efdfd96a4222818f724
Heber Sandoval <he3ers@gmail.com>
2026-01-09 11:12:41 -0600

    Created SignIn page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
             </Routes>
         </BrowserRouter>
     );
