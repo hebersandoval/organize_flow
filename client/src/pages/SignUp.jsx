37aff850672fe94c3c6c5262dd077a386d99ee9b
Heber Sandoval <he3ers@gmail.com>
2026-01-10 20:09:56 -0600

    Imported useForm to register and check form state.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,11 @@
+import { useForm } from 'react-hook-form';
+
 export default function SignUp() {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting },
+    } = useForm();
+
     return <h1>Sign up</h1>;
 }
