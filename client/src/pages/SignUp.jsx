285786d3ad42b4f663110afd099c714b7900b9b8
Heber Sandoval <he3ers@gmail.com>
2026-01-10 20:22:25 -0600

    Created a Box component with nested elements to style the form.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,4 +1,6 @@
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { FormControl, Input, Button, Text, Box, Flex, Heading, Stack, FormErrorMessage } from '@chakra-ui/react';
 
 export default function SignUp() {
     const {
@@ -7,5 +9,22 @@ export default function SignUp() {
         formState: { errors, isSubmitting },
     } = useForm();
 
-    return <h1>Sign up</h1>;
+    return (
+        <Box p="3" maxW="lg" mx="auto">
+            <Heading as="h1" textAlign="center" fontSize="3xl" fontWeight="semibold" my="7">
+                Create an account
+            </Heading>
+
+            <form>{/* form details */}</form>
+
+            <Flex gap="2" mt="5">
+                <Text>Have an account?</Text>
+                <Link to={'/signin'}>
+                    <Text as="span" color="blue.400">
+                        Sign in
+                    </Text>
+                </Link>
+            </Flex>
+        </Box>
+    );
 }
