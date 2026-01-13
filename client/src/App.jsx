de93e79ed16c95cc6a8b3b34bdedb0b49356c5f0
Heber Sandoval <he3ers@gmail.com>
2026-01-13 10:47:19 -0600

    Imported Toast for notification alerts.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Toaster } from 'react-hot-toast';
 
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
@@ -9,6 +10,7 @@ function App() {
     return (
         <ChakraProvider>
             <BrowserRouter>
+                <Toaster position="bottom-right" />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/signin" element={<SignIn />} />
