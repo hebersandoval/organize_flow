70ee1018eb3b514c168750fe30d454672616ba40
Heber Sandoval <he3ers@gmail.com>
2026-01-15 10:55:39 -0600

    Imported <UserProvider> so any component inside of it can access the context.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Toaster } from 'react-hot-toast';
 
+// Local imports
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { UserProvider, useUser } from './context/UserContext';
 
 function App() {
     return (
-        <ChakraProvider>
-            <BrowserRouter>
-                <Toaster position="bottom-right" />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/signup" element={<SignUp />} />
-                </Routes>
-            </BrowserRouter>
-        </ChakraProvider>
+        <UserProvider>
+            <ChakraProvider>
+                <BrowserRouter>
+                    <Toaster position="bottom-right" />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/signin" element={<SignIn />} />
+                        <Route path="/signup" element={<SignUp />} />
+                    </Routes>
+                </BrowserRouter>
+            </ChakraProvider>
+        </UserProvider>
     );
 }
 
