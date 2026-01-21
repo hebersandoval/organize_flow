01de3f15497415320b72a31b0dd668969e9810f7
Heber Sandoval <he3ers@gmail.com>
2026-01-21 11:13:35 -0600

    Implemented logic for signin and when form is submitted form values are sent on to the request body.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,3 +1,45 @@
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { FormControl, Input, Button, Text, Box, Flex, Heading, Stack, FormErrorMessage } from '@chakra-ui/react';
+import toast from 'react-hot-toast';
+
+// Local imports
+import { API_BASE_URL } from '../util.js';
+import { useUser } from '../context/UserContext.jsx';
+
 export default function SignIn() {
-    return <h1>Sign in</h1>;
+    const navigate = useNavigate();
+    const { updateUser } = useUser();
+
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting },
+    } = useForm();
+
+    const doSubmit = async (values) => {
+        try {
+            const res = await fetch(`${API_BASE_URL}/auth/signin`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify(values),
+            });
+
+            const data = await res.json();
+
+            if (res.status === 200) {
+                toast.success('Sign in successful.');
+                updateUser(data);
+                navigate('/profile');
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error('Something went wrong.');
+        }
+    };
 }
